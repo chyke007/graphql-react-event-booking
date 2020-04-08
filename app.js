90451a4fa3aa36221e91bffc38b39b79b6cfc70c
chyke007 <chibuike@zercomsystems.com>
2020-04-08 22:26:53 +0100

    Integrated mongoDb

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp =require('express-graphql')
 const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
+
+const Event = require('./models/event');
 
 const app = express();
 
@@ -9,12 +12,26 @@ app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation{
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -24,15 +41,39 @@ app.use('/graphql', graphqlHttp({
     `),
     rootValue: {
         events: () => {
-            return ['Romantic Cooking','Sailing','All-Night Coding']
+            return Event.find().then(events => {
+                return events.map(event => {
+                    return {...event._doc, _id: event.id}
+                })
+            }).catch(err => {
+                throw err
+            })
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date)
+            });
+            return event.save().then(result => {
+                console.log(result);
+                return {...result._doc, _id: result._doc._id.toString()}
+            }).catch(err => {
+                console.log(err);
+                throw err
+            });
+            
         }
     },
     graphiql:true
 
 }));
 
-app.listen(3000);
+console.log(`${process.env.DB_OPTIONS}`)
+mongoose.connect(`mongodb://${process.env.DB_URL}/${process.env.MONGO_DB}`, {useNewUrlParser:true}).then(() => {
+    app.listen(3000);
+}).catch((err) => {
+    console.log(err) 
+})
